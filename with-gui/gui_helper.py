a8377d2e9b2de1d4dd2bd1fc3fa05e0013aeafc7
Erdem Aybek <eaybek@gmail.com>
2014-12-09 00:48:32 +0200

    create helpertry for test and update gui_helper

--- with-gui/gui_helper.py
+++ with-gui/gui_helper.py
@@ -4,131 +4,133 @@ from nymph.Nymph import *
 
 
 ###
-#	helper is the object interface for GDManager 
-#	its responsible comminication between GDManager and user class
+#       helper is the object interface for GDManager 
+#       its responsible comminication between GDManager and user class
 ###
 class helper(nymph):
     # helper is a nymph
     def __init__(self,nymphdata):
-        super.(helper,self).__init__(nymphdata)
+        super(helper,self).__init__(nymphdata)
     
     #listen is processes coming data to other nymph
     #
     #parameters:
-    #	words
+    #   words
     #
     #return:
     def listen(self,words):
-    	data=json.loads(open('data.json','r').read())
-    	func={
-    	    'read_OK':read_handler,
-    	    'upload_OK':upload_Handler,
-    	    'get_authorize_url_OK':get_authorize_url_Handler,
-    	    'set_credentials_OK':set_credentials_Handler,
-    	    'download_OK':download_Handler,
-    	    'sendMessage_OK':sendMessage_Handler,
-    	    'setNode_OK':setNode_Handler,    	    
-    	    'error_OK':error_Manager_Handler,
-    	}
-    	if(words in func):
-    	    func[words](data)
-    	else:
-    	    error_Manager_Handler()
-    	print(words)
-    	
+        data=json.loads(open('data.json','r').read())
+        func={
+            'read_OK':read_handler,
+            'upload_OK':upload_Handler,
+            'get_authorize_url_OK':get_authorize_url_Handler,
+            'set_credentials_OK':set_credentials_Handler,
+            'download_OK':download_Handler,
+            'message_OK':listen_message_Handler,  
+            'url_OK':listen_url_Handler,          
+            'error_OK':error_Manager_Handler,
+        }
+        if(words in func):
+            func[words](data)
+        else:
+            error_Manager_Handler('')
+        print(words)
+        
+
+    
+    def listen_url_Handler(self,data):
+        pass
+    
+    def listen_message_Handler(self,data):
+        pass
+    
+    
     def read_Handler(self,data):
-    	pass
+        pass
     
     def upload_Handler(self,data):
-    	pass
-    	
+        pass
+        
     def get_authorize_url_Handler(self,data):
-    	pass
-    		
+        pass
+                
     def set_credentials_Handler(self,data):
-    	pass
+        pass
     
     def download_Handler(self,data):
-    	pass
-    	
+        pass
+        
     def sendMessage_Handler(self,data):
-    	pass
+        pass
     
     def setNode_Handler(self,data):
-    	pass
+        pass
     
     def error_Manager_Handler(self,data):
-    	pass
+        pass
     #sayFormat processes sending data to other nymph
     #
     #parameters:
-    #	words
+    #   words
     #
     #return:
     def sayFormat(self,words):
-    	return words
+        return words
     
     #read trigger read proccess and return results
     #
     #parameters:
-    #	words
+    #   words
     #
     #return:
     def read(self):
-    	self.say("read")
-    	
+        self.say('{ "query": "read" }')
+        
     #upload trigger upload proccess for filename 
     #
     #parameters:
-    #	filename
-    #	is_share
+    #   filename
+    #   is_share
     #
     #return:
-    def upload(self,filename,is_share):
-    	self.say("upload")
-    	
+    def upload(self,filename,is_share):    
+        self.say('{ "query": "upload", "0": "' + filename + '", "1": "' + is_share + '" }')
+        
     #return an authorize url
     #
     #parameters:
     #
     #return:
     def get_authorize_url(self):
-    	self.say("get_authorize_url")
-    	
-    	
+        '{ "query": "get_authorize_url" }'
+        self.say("get_authorize_url")
+        
+        
     #set the credentials  
     #
     #parameters:
-    #	authorization_code
+    #   authorization_code
     #
     #return:
     def set_credentials(self, authorization_code):
-    	self.say("set_credentials")
-    	
-    #download the url	
+        self.say('{ "query": "set_credentials", "0": "' + authorization_code + '" }')
+        
+    #download the url   
     #
     #parameters:
-    #	url
+    #   url
     #
     #return:
     def download(self,url):
-    	self.say("download")
+        self.say('{ "query": "download", "0": "'+url+'" }')
     
-    #sends Message to Other GDManager Node
-    #
-    #parameters:
-    #	message
-    #
-    #return:
-    def sendMessage(self,message):
-    	self.say("download")
-    	
+        
     #set the Other GDManager Node
     #
     #parameters:
-    #	nymphdata
+    #   nymphdata
     #
     #return:
-    def setNode(self,nymphdata):
-    	self.say("setNode")
-    	
+    def talk(self,nymphdata,message):
+        self.say('{ "query": "talk", "0": "'+nymphdata.NAME+'", "1": "'+nymphdata.HOST+'", "2": "'+nymphdata.PORT+'", "3": "'+message+'" }')
+        
